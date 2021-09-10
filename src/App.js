40314bd2e62b2371ca60bbddc624de4dea4bc618
Sebastian <sebas6292@gmail.com>
2021-09-10 13:32:18 -0700

    that class component was a curve ball

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
+import { connect } from './react-redux';
+
 import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
+import { getSmurfs } from './actions/index';
 
 class App extends Component {
+  state = {
+    smurfs: [], 
+    loading: false, 
+    error: ''
+  }
   componentDidMount() {
     axios.get('http://localhost:3333/smurfs')
     // console.log(res)
@@ -30,8 +38,15 @@ class App extends Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    murfs: state.murfs,
+    loading: state.loading,
+    error: state.error
+  }
+}
 
-export default App;
+export default connect(mapStateToProps, { getSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
